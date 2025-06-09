47f4157c016da6577cecd11406dd39018c958483
student210 <student210@stduent210>
2025-06-09 14:56:05 +0300

    rebase2

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def handle_send_message(data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, debug=True)
